992f3041ae586f7e0b207006f788a01ff2a4f29c
SkylerAuraArena <skylerauraarena@gmail.com>
2022-06-15 21:28:27 +0200

    Correction de la bdd btn du dataHelpers

--- src/components/helpers/dataHelpers.ts
+++ src/components/helpers/dataHelpers.ts
@@ -2,14 +2,18 @@ import { IButtonProps } from "./interfacesHelpers";
 
 export let nameList:Array<IButtonProps> = [{
     title:'Mathématiques',
-    color:'bg-yellow-500'
+    color:'bg-yellow-500',
+    func: () => {}
 }, {
     title:'Français',
-    color:'bg-blue-500'
+    color:'bg-blue-500',
+    func: () => {}
 }, {
     title:'Dactylographie',
-    color:'bg-slate-500'
+    color:'bg-slate-500',
+    func: () => {}
 }, {
     title:'Langues étrangères',
-    color:'bg-green-500'
+    color:'bg-green-500',
+    func: () => {}
 }]
